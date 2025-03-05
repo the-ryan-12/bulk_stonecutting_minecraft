32c3eb0ed2b0338bb412a13781765e4ed52f9a31
the-ryan-12 <ryanlowell12@gmail.com>
2025-03-05 15:03:17 -0500

    Remove unused functions

--- src/client/java/org/vee/bulk_stonecutting/client/ResizableCheckbox.java
+++ src/client/java/org/vee/bulk_stonecutting/client/ResizableCheckbox.java
@@ -1,7 +1,5 @@
 package org.vee.bulk_stonecutting.client;
 
 public interface ResizableCheckbox {
-    void bulk_stonecutting$setScale(float scale);
-    float bulk_stonecutting$getScale();
     void bulk_stonecutting$needsResize(boolean resize);
 }
